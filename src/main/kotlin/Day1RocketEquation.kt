d263ade8bf4e2e371383748fb20db9eccc18c9ec
rebeckao <rebecka.oefverholm@gmail.com>
2019-12-01 12:18:58 +0100

    Day 1 part 2

--- src/main/kotlin/Day1RocketEquation.kt
+++ src/main/kotlin/Day1RocketEquation.kt
@@ -1,9 +1,32 @@
+import java.lang.Integer.max
+
 class Day1RocketEquation {
-    fun fuelRequirements(masses: List<Int>): Int {
+    fun fuelRequirementsForMass(masses: List<Int>): Int {
         return masses.stream()
             .mapToInt(Int::toInt)
             .map { it / 3 }
             .map { it - 2 }
             .sum();
+    }
+
+    fun totalFuelRequirements(masses: List<Int>): Int {
+        return masses.stream()
+            .mapToInt(Int::toInt)
+            .map(this::totalFuelRequirement)
+            .sum()
+    }
+
+    fun totalFuelRequirement(mass: Int): Int {
+        var extraFuel = fuelRequirementForMass(mass)
+        var totalFuelRequirement = extraFuel
+        while (extraFuel > 0) {
+            extraFuel = max(0, fuelRequirementsForMass(listOf(extraFuel)))
+            totalFuelRequirement += extraFuel
+        }
+        return totalFuelRequirement
+    }
+
+    fun fuelRequirementForMass(mass: Int): Int {
+        return mass/3 -2;
     }
 }
